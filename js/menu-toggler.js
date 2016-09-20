933962b3714b24eabc661a2ad44eb365d53e72d7
jkikksa <jkikksa@gmail.com>
2016-09-21 00:05:49 +0300

    реализовал постоянно открытое меню при отсутсвии js

--- js/menu-toggler.js
+++ js/menu-toggler.js
@@ -1,6 +1,8 @@
 var toggler = document.querySelector(".main-menu__toggler");
 var menu = document.querySelector(".main-menu");
 
+menu.classList.remove("main-menu--no-js");
+
 toggler.addEventListener("click", function(event) {
   event.preventDefault();
   if (menu.classList.contains("main-menu--closed")) {
