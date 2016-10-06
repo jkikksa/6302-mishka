131adf8eb6502c228d910e77905ede02dd97bf8e
Dmitry Rusakov <jkikksa@gmail.com>
2016-10-06 15:29:16 +0300

    добавил карту

---
+++ src/js/map.js
@@ -0,0 +1,15 @@
+function initMap() {
+  var myLatLng = {lat: 59.936350, lng: 30.321120};
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: {lat: 59.936343, lng: 30.321770},
+    zoom: 16,
+    disableDefaultUI: true
+  });
+  var image = "img/icon-map-pin.png";
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    icon: image,
+    title: "Mishka"
+  });
+};
