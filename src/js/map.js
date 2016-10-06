b98a0c7d4297fbe2a147299aebf93dd5ddfaebfd
Dmitry Rusakov <jkikksa@gmail.com>
2016-10-06 15:38:15 +0300

    мелкий фикс

--- src/js/map.js
+++ src/js/map.js
@@ -2,8 +2,7 @@ function initMap() {
   var myLatLng = {lat: 59.936350, lng: 30.321120};
   var map = new google.maps.Map(document.getElementById("map"), {
     center: {lat: 59.936343, lng: 30.321770},
-    zoom: 16,
-    disableDefaultUI: true
+    zoom: 16
   });
   var image = "img/icon-map-pin.png";
   var marker = new google.maps.Marker({
